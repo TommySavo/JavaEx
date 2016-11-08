3ac5ee88db1b954ae994923097a1c95b2f2a1310
Unreal4project <Unreal4project@gmail.com>
2016-11-08 19:02:00 +0100

    Added Runnable interface

--- JavaThreads/src/javathreads/TestMatrice.java
+++ JavaThreads/src/javathreads/TestMatrice.java
@@ -9,13 +9,18 @@ package javathreads;
  *
  * @author Lorenzo
  */
-public class TestMatrice {
+public class TestMatrice implements Runnable {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
