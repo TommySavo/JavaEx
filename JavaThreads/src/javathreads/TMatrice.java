4121f979bebfaf6882b0168408f586a93e3d203d
Unreal4project <Unreal4project@gmail.com>
2016-11-08 20:29:18 +0100

    Completed TMatrice class

--- JavaThreads/src/javathreads/TMatrice.java
+++ JavaThreads/src/javathreads/TMatrice.java
@@ -10,5 +10,16 @@ package javathreads;
  * @author Lorenzo
  */
 public class TMatrice {
-    
+    public int matrice[][];
+    public int N,M;
+    public TMatrice(int r,int c){
+        N=r;
+        M=c;
+        matrice = new int[N][M];
+        for(int i = 0;i<N;i++){
+            for(int j = 0; j<M; j++){
+                matrice[i][j]=(int)(Math.random()*10);
+            }
+        }
+    }
 }
